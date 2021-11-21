48eeb79450932c2035b77f3b0612215dd6eb0b66
ASUS <safar2049@gmail.com>
2021-11-21 18:25:35 +0300

    added a check when importing a class

--- part_1/main.py
+++ part_1/main.py
@@ -1,14 +1,17 @@
 from bot_class import Instagram_bot
 from auth_data import username, password
 
-my_bot = Instagram_bot(username,password)
-my_bot.login()
-
-my_bot.like_photo_by_hashtag('sketch')
+if __name__ == '__main__':
 
 
+    my_bot = Instagram_bot(username,password)
+    my_bot.login()
 
-my_bot.browser_close()
+    my_bot.like_photo_by_hashtag('sketch')
+
+
+
+    my_bot.browser_close()
 
 
 
